c08c901d5576138820b4b7060daf6a1e81f656bf
Shakiba <shakibaalam092@gmail.com>
2024-07-09 22:04:02 +0600

    cors origin

--- final_xm_phibook/settings.py
+++ final_xm_phibook/settings.py
@@ -85,10 +85,15 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
+
+CSRF_TRUSTED_ORIGINS = ['https://phibook-backend.onrender.com','https://*.127.0.0.1','https://phibook-80995.web.app']
+
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "https://phibook-backend.onrender.com",
+    "https://phibook-80995.web.app/"
 ]
 
 ROOT_URLCONF = 'final_xm_phibook.urls'
